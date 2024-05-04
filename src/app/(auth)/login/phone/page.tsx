7a2e4882b1379fb7d5c6503a25da80a6c285ca56
magoumi <agoumihunter@gmail.com>
2024-05-04 17:19:20 +0100

    - add suspense to useSearchParams to fix deployment #[https://nextjs.org/docs/messages/missing-suspense-with-csr-bailout]

--- src/app/(auth)/login/phone/page.tsx
+++ src/app/(auth)/login/phone/page.tsx
@@ -5,11 +5,20 @@ import PhoneInput from "@/ui/form/phone-input";
 import Button from "@/ui/form/button";
 import ToggleInput from "@/ui/form/toggle-input";
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
-export default function Phone() {
+function ButtonWrapper() {
     const searchParams = useSearchParams();
     const intent = searchParams.get('intent');
 
+    return (
+        <>
+            {!intent && <ToggleInput style="basis-5 float-left"/>}
+        </>
+    );
+}
+
+export default function Phone() {
     return (
         <>
             <Navbar logoHeight='36px'/>
@@ -18,7 +27,9 @@ export default function Phone() {
                     <PhoneInput name='phone' placeholder='Phone number'/>
                     <div className="basis-full mt-5">
                         <div className="button m-auto w-96">
-                            {!intent && <ToggleInput style="basis-5 float-left"/>}
+                            <Suspense>
+                                <ButtonWrapper/>
+                            </Suspense>
                             <Button text='Next' style="w-15 float-right" marginBlockStart="0"/>
                         </div>
                     </div>
